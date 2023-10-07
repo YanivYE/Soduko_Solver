75ff6baffdea5d0f290b1806136d541bc0837412
YanivYE <yanivye106@gmail.com>
2023-10-07 17:27:14 +0300

    finding the biggest contour

--- soduko_solver.py
+++ soduko_solver.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 PATH = "board.png"
 SIZE = 450
@@ -16,6 +17,26 @@ def define_contours(img, preprocessed_img):
     img_big_contour = img.copy()
     contours, hierarchy = cv2.findContours(preprocessed_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(img_contours, contours, -1, (0, 255, 0), 3)
+    biggest_contour, max_area = find_biggest_contour(contours)
+    if biggest_contour.size != 0:
+        biggest_contour = reorder(biggest_contour)
+
+
+def find_biggest_contour(contours):
+    biggest_contour = np.array([])
+    max_area = 0
+    for con in contours:
+        area = cv2.contourArea(con)
+        if area > 50:
+            p = cv2.arcLength(con, True)
+            approx = cv2.approxPolyDP(con, 0.02 * p, True)
+            if area > max_area and len(approx) == 3:
+                biggest_contour = approx
+                max_area = area
+    return biggest_contour, max_area
+
+def reorder(points_arr):
+    
 
 
 def main():
