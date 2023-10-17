a139547562f1e51a4fc2720411176e1110759966
YanivYE <yanivye106@gmail.com>
2023-10-17 12:43:11 +0300

    added contours defenition functionality

--- Extract_Board.py
+++ Extract_Board.py
@@ -5,8 +5,10 @@ import numpy as np
 import imutils
 import cv2
 
+DEBUG = True
 
-def find_board(image, debug=True):
+
+def define_board(image):
     # convert the image to grayscale and blur it slightly
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (7, 7), 3)
@@ -18,8 +20,48 @@ def find_board(image, debug=True):
 
     # check to see if we are visualizing each step of the image
     # processing pipeline (in this case, thresholding)
-    if debug:
+    if DEBUG:
         cv2.imshow("Board Thresh", thresh)
         cv2.waitKey(0)
-    
 
+
+
+
+
+def find_board_contour(thresh_img, image):
+    # initialize a contour that corresponds to the puzzle outline
+    board_cnt = None
+
+    # find contours in the thresholded image and sort them by size in
+    # descending order
+    cnts = cv2.findContours(thresh_img.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+
+    # loop over the contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # if our approximated contour has four points, then we can
+        # assume we have found the outline of the puzzle
+        if len(approx) == 4:
+            board_cnt = approx
+            break
+
+    # if the puzzle contour is empty then our script could not find
+    # the outline of the Sudoku puzzle so raise an error
+    if board_cnt is None:
+        raise Exception(("Could not find Sudoku puzzle outline. "
+                         "Try debugging your thresholding and contour steps."))
+        # check to see if we are visualizing the outline of the detected
+        # Sudoku puzzle
+
+    if DEBUG:
+        # draw the contour of the puzzle on the image and then display
+        # it to our screen for visualization/debugging purposes
+        output = image.copy()
+        cv2.drawContours(output, [board_cnt], -1, (0, 255, 0), 2)
+        cv2.imshow("Puzzle Outline", output)
+        cv2.waitKey(0)
