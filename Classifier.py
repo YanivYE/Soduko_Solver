cde6a1c7eb10301906f2b5f0083a7114fbc8afd0
YanivYE <yanivye106@gmail.com>
2023-10-17 13:31:09 +0300

    building cell location classifier

--- Classifier.py
+++ Classifier.py
@@ -21,11 +21,28 @@ image = imutils.resize(image, width=600)
 (puzzleImage, warped) = Extract_Puzzle.define_board(image)
 # initialize our 9x9 Sudoku board
 board = np.zeros((9, 9), dtype="int")
+
 # a Sudoku puzzle is a 9x9 grid (81 individual cells), so we can
 # infer the location of each cell by dividing the warped image
 # into a 9x9 grid
-stepX = warped.shape[1] // 9
-stepY = warped.shape[0] // 9
+step_X = warped.shape[1] // 9
+step_Y = warped.shape[0] // 9
 # initialize a list to store the (x, y)-coordinates of each cell
 # location
 cell_locs = []
+
+
+def initialize_cell_locations(stepX, stepY):
+    # loop over the grid locations
+    for y in range(0, 9):
+        # initialize the current list of cell locations
+        row = []
+        for x in range(0, 9):
+            # compute the starting and ending (x, y)-coordinates of the
+            # current cell
+            startX = x * stepX
+            startY = y * stepY
+            endX = (x + 1) * stepX
+            endY = (y + 1) * stepY
+            # add the (x, y)-coordinates to our cell locations list
+            row.append((startX, startY, endX, endY))
